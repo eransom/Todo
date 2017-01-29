47be0373e83c6b5c5bd807dad5ed69b4f50aa1da
Eric Ransom <ericransom@Erics-MBP.cfl.rr.com>
2017-01-29 17:01:58 -0500

    fixed Firebase sync

--- src/App.js
+++ src/App.js
@@ -6,14 +6,12 @@ class App extends Component {
   constructor () {
     super()
     this.state = {
-      userName: "",
       toDo: []
     }
-    this.auth = base.auth()
   }
 
-  componentDidmount () {
-   base.syncState('toDo', {
+  componentDidMount () {
+   this.sync = base.syncState('toDo', {
    state: 'toDo',
    context: this,
    asArray: true
@@ -23,7 +21,10 @@ class App extends Component {
   addToDo (e) {
     if(e.keyCode === 13) {
     let text = this.input.value.trim()
-    let newToDo = {text: text}
+    let newToDo = {
+      text: text,
+      complete: false
+    }
     let newToDoArray = this.state.toDo.concat(newToDo)
     this.setState({
       toDo: newToDoArray
@@ -42,79 +43,89 @@ class App extends Component {
  }
 
 
-// completeItem(completeItem) {
-//  var newTodos = this.state.todos.map(item => {
-//    if (item !== completeItem) {
-//      return item
-//    } else {
-//      item.complete = !item.completeItem
-//      return item
-//    }
-//  })
-//  this.setState ({
-//    todos:newTodos
-//  })
-// }
-//
+  completeItem(completedToDo) {
+   var newTodos = this.state.toDo.map(item => {
+     if (item !== completedToDo) {
+       return item
+     } else {
+       item.complete = !item.complete
+      console.log('here is: ', item)
+       return item
+     }
+   })
+   this.setState ({
+     toDo:newTodos
+   })
+   console.log('after setting state: ', this.state.toDo)
+  }
 
-// editItems () {
-//  if (this.state.classFill === "") {
-//    this. setState({classFill: 'editing'})
-//  }
-// }
+
+  // editItems () {
+  //  if (this.state.classFill === "") {
+  //    this. setState({classFill: 'editing'})
+  //  }
+  // }
+
+  clearCompleteCheck () {
+    if (this.state.toDo.some(toDo => toDo.complete === true)) {
+      return <button className="clear-completed">Clear completed</button>
+    }
+  }
+
+  // clearAllComplete() {
+  //   if (this.state.toDo)
+  // }
 
 
   render() {
+    console.log(this.state)
     return (
-        <section className="todoapp">
-          <header className="header">
-            <h1>todos</h1>
-            <input onKeyUp={this.addToDo.bind(this)} className="new-todo" placeholder="What needs to be done?" ref={element => this.input = element} autofocus />
-          </header>
-          <section className="main">
-            <input className="toggle-all" type="checkbox" />
-            <label for="toggle-all">Mark all as complete</label>
-            <ul className="todo-list">
+
+      <section className="todoapp">
+        <header className="header">
+          <h1>todos</h1>
+          <input onKeyUp={this.addToDo.bind(this)} className="new-todo" placeholder="What needs to be done?" ref={element => this.input = element} autoFocus />
+        </header>
+
+        <section className="main">
+
+          <input className="toggle-all" type="checkbox" />
+          <label htmlFor="toggle-all">Mark all as complete</label>
+
+          <ul className="todo-list">
             {this.state.toDo.map((todo, index) => {
-              return <li key={index}>
-                <div className="view">
-                  <input className="toggle" type="checkbox" checked />
-                  <label>{todo.text}</label>
-                  <button ref={button => this.button = button}
-                  onClick={this.deleteItem.bind(this)} className="destroy"></button>
-                </div>
-                <input className="edit" value="Create a TodoMVC template" />
-              </li>
+            return <li key={index}>
+            <div className="view">
+              <input onClick={() => this.completeItem(todo)} className="toggle" type="checkbox" defaultChecked={todo.complete} />
+              <label>{todo.text}</label>
+              <button ref={button => this.button = button}
+              onClick={this.deleteItem.bind(this)} className="destroy"></button>
+            </div>
+            <input className="edit" value="Create a TodoMVC template" />
+            </li>
              })
            }
-              <li>
-                <div className="view">
-                  <input className="toggle" type="checkbox" />
-                  <label>Buy a unicorn</label>
-                  <button onClick={this.deleteItem.bind(this)} className="destroy"></button>
-                </div>
-                <input className="edit" value="Rule the web" />
-              </li>
-            </ul>
-          </section>
-          <footer className="footer">
-            <span className="todo-count"><strong>0</strong> item left</span>
-            <ul className="filters">
-              <li>
-                <a className="selected" href="#/">All</a>
-              </li>
-              <li>
-                <a href="#/active">Active</a>
-              </li>
-              <li>
-                <a href="#/completed">Completed</a>
-              </li>
-            </ul>
-            <button className="clear-completed">Clear completed</button>
-          </footer>
+          </ul>
         </section>
-      )
-    }
+
+        <footer hidden={this.state.toDo.length === 0} className="footer">
+          <span  className="todo-count"><strong>{this.state.toDo.length}</strong> item left</span>
+          <ul className="filters">
+            <li>
+              <a className="selected" href="#/">All</a>
+            </li>
+            <li>
+              <a href="#/active">Active</a>
+            </li>
+            <li>
+              <a href="#/completed">Completed</a>
+            </li>
+          </ul>
+          {this.clearCompleteCheck()}
+        </footer>
+      </section>
+    )
   }
+}
 
 export default App;
